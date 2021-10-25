69666a746098ba9871adf26eba5f338e7e6728a6
bin6246 <bin6246@naver.com>
2021-10-25 17:22:14 +0900

    [S05P31C104-90][BE] ci : cors 해제

--- backend/src/main/java/iroz/backend/config/WebMvcConfig.java
+++ backend/src/main/java/iroz/backend/config/WebMvcConfig.java
@@ -81,5 +81,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*");
+                
     }
 }
