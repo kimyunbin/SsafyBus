76fd2e959003d03b760330daaebb58b8338911fe
Gwanghun-Im <lgh82229047@gmail.com>
2021-11-01 15:46:13 +0900

    [S05P31C104-50][BE] fix: 헬프게시판 response 수정

--- backend/src/main/java/iroz/backend/db/Mapping/HelpAllMapping.java
+++ backend/src/main/java/iroz/backend/db/Mapping/HelpAllMapping.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 public interface HelpAllMapping {
     String getTitle();
+    Long getId();
     UserInfoMapping getUser();
     String getContent();
     LocalDateTime getCreatedAt();
