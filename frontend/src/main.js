b07c8b04056edfcf397e0cd7ba8fe1a91ec00b10
Gwanghun-Im <lgh82229047@gmail.com>
2021-11-05 01:20:12 +0900

    [S05P31C104-82][BE] feat:오픈비두 임시

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from '@/store/index.js'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
-Vue.use(Vuetify);
 
 new Vue({
   router,
